d25e7b46dfa51769534de9ebaeb7890d8a542c7b
Shin Min Khant <133580286+ShinMinKhant@users.noreply.github.com>
2023-12-21 22:41:54 +0630

    Upload Project

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+pip install gunicorn
+pip install django
+pip install Pillow
+pip install dj-database-url
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
